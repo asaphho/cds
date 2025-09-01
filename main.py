ecee06bb8c3b1e6161c901631920cb7fe909bf5a
Asaph Ho <asaphho@yahoo.com.sg>
2025-09-01 23:10:50 +0800

    Made the main script begin dev milestone exercise for now

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from utils.qn_builder import begin_dev_milestone_exercise
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    begin_dev_milestone_exercise()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
